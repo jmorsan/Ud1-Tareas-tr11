c625ac9385d629b2d38811d71046656f4250441f
jmorsan426 <jmorsan426@g.educaand.es>
2022-09-28 12:08:12 +0200

    Cambio

--- src/main/java/ies/jms/Tarea6/Tarea6.java
+++ src/main/java/ies/jms/Tarea6/Tarea6.java
@@ -1,18 +1,30 @@
 package ies.jms.Tarea6;
 
+import javax.swing.*;
 import java.io.File;
+import java.sql.SQLOutput;
 import java.util.Arrays;
 
 public class Tarea6 {
     public static void main(String[] args) {
+        try{
+            File miCarpeta = new File("C:" + File.separator+"Users" + File.separator+"2DAM" + File.separator+"Desktop" + File.separator+"Carpeta prueba");
 
-        File miCarpeta = new File("C:" + File.separator+"Users" + File.separator+"2DAM" + File.separator+"Desktop" + File.separator+"Carpeta prueba");
+            File[] misElementos = miCarpeta.listFiles(new Filtro());
 
-        File[] misElementos = miCarpeta.listFiles(new Filtro());
+            for (File entrada : misElementos)
+            {
+                System.out.println(entrada.getName());
 
-        for (File entrada : misElementos)
-        {
-            System.out.println(entrada.getName());
+            }
+
+        }catch (NullPointerException e){
+            System.out.println(e.getStackTrace());
+            System.out.println("No se encontro ningun archivo de ese tipo");
+
+        }catch (Exception e){
+
+            System.out.println(e.getStackTrace());
 
         }
 
